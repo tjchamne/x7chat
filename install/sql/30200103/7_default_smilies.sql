53b6665bf3bce6431af8dadc50a8697e6acbc259
Tim Chamness <tim.chamness@upandrunningsoftware.com>
2013-03-26 22:43:26 -0500

    adding default smilies on install

---
+++ install/sql/30200103/7_default_smilies.sql
@@ -0,0 +1,14 @@
+INSERT INTO `{$prefix}smilies` (`id`, `token`, `image`) VALUES
+(1, ':wink:', 'smilies/ani_wink.gif'),
+(2, 'o_o', 'smilies/big_eyes.gif'),
+(3, '8|', 'smilies/cool.gif'),
+(4, ':''(', 'smilies/cry.gif'),
+(5, ':roll:', 'smilies/eye_roll.gif'),
+(6, ':D', 'smilies/grin.gif'),
+(7, ':)', 'smilies/happy.gif'),
+(8, '-_-', 'smilies/not_impressed.gif'),
+(10, ':p', 'smilies/stickout.gif'),
+(11, ':|', 'smilies/straight.gif'),
+(12, ':o', 'smilies/surprised.gif'),
+(13, ':(', 'smilies/unhappy.gif'),
+(14, ':\\', 'smilies/wink.gif');
