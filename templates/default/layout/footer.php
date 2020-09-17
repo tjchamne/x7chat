6fd42737d09f4b1b3e72c7b5e33be6b131df6069
root <sleepingkkrby@gmail.com>
2020-09-17 14:38:32 +0800

    changing the footer and the styles so that the footer doesn't interfere with the resizing the chat window

--- templates/default/layout/footer.php
+++ templates/default/layout/footer.php
@@ -1,11 +1,12 @@
 					</div>
 				</div>
 			</div>
-			<div id="page_footer">
-				<div id="footer_inner">
-					<a href="http://www.x7chat.com/" target="_blank">Powered By X7 Chat</a>
-				</div>
+		</div>
+		<div id="page_footer">
+			<div id="footer_inner">
+				<a href="http://www.x7chat.com/" target="_blank">Powered By X7 Chat</a>
 			</div>
 		</div>
+
 	</body>
 </html>
